b4334ab04aa3179e6af8aae4a7220c41a3164dac
Location <jamesabcde277@gmail.com>
2023-07-07 16:41:39 +0800

    add invert tree method

--- binary_tree.php
+++ binary_tree.php
@@ -11,5 +11,18 @@ class TreeNode {
     }
 }
 
+class Solution {
+    function invertTree($root) {
+        if($root==null){
+            return null;
+        }
+        $temp=$root->left;
+        $root->left=$root->right;
+        $root->right=$temp;
+        $this->invertTree($root->left);
+        $this->invertTree($root->right);
+        return $root;
+    }
+}
 
 ?>
