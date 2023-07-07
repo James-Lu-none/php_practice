6556dc481530a4814289b3fb93ce0d142e5e13a5
Location <jamesabcde277@gmail.com>
2023-07-07 16:40:44 +0800

    add tree structure

--- binary_tree.php
+++ binary_tree.php
@@ -1,4 +1,15 @@
 <?php
 //simple binary tree practice
+class TreeNode {
+    public $val = null;
+    public $left = null; 
+    public $right = null;
+    function __construct($val = 0, $left = null, $right = null) {
+        $this->val = $val;
+        $this->left = $left;
+        $this->right = $right;
+    }
+}
+
 
 ?>
