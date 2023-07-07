ceddbf7509e9ca2242c505add4978444b61c62e6
Location <jamesabcde277@gmail.com>
2023-07-07 16:51:03 +0800

    remove duplicated class

--- binary_tree.php
+++ binary_tree.php
@@ -11,17 +11,6 @@ class TreeNode {
     }
 }
 
-class TreeNode {
-    public $val = null;
-    public $left = null; 
-    public $right = null;
-    function __construct($val = 0, $left = null, $right = null) {
-        $this->val = $val;
-        $this->left = $left;
-        $this->right = $right;
-    }
-}
-
 class Solution {
     function buildTree($array) {//map array into tree
         if (empty($array)) {
