f363ef2ecdcd0f2b20a771792e95f4198d863983
Location <jamesabcde277@gmail.com>
2023-07-07 16:42:39 +0800

    add build/unbuild tree method

--- binary_tree.php
+++ binary_tree.php
@@ -11,7 +11,70 @@ class TreeNode {
     }
 }
 
+class TreeNode {
+    public $val = null;
+    public $left = null; 
+    public $right = null;
+    function __construct($val = 0, $left = null, $right = null) {
+        $this->val = $val;
+        $this->left = $left;
+        $this->right = $right;
+    }
+}
+
 class Solution {
+    function buildTree($array) {
+        if (empty($array)) {
+            return null;
+        }
+        $root = new TreeNode(array_shift($array));
+        $queue = [$root]; // Initialize a queue with the root node
+        // echo json_encode($array)."\n";
+        while (!empty($array) && !empty($queue)) {
+            $currentNode = array_shift($queue);
+
+            if($currentNode->val==null) continue;
+
+            $newNode= new TreeNode(array_shift($array));
+            // echo json_encode($array)."\n";
+            $currentNode->left= $newNode;
+            array_push($queue,$newNode);
+
+            if (!empty($array)) {
+                $newNode= new TreeNode(array_shift($array));
+                // echo json_encode($array)."\n";
+                $currentNode->right= $newNode;
+                array_push($queue,$newNode);
+            }
+        }
+        return $root;
+    }
+
+    function unbuildTree($root){
+        if ($root == null) {
+            return [];
+        }
+        $result = [$root->val];
+        $queue = [$root];
+
+        while (!empty($queue)){
+            $currentNode = array_shift($queue);
+            $leftNode=$currentNode->left;
+            $rightNode=$currentNode->right;
+
+            if($leftNode!=null){
+                array_push($queue,$leftNode);
+                array_push($result,$leftNode->val);
+            }
+            else array_push($result,null);
+            if($rightNode!=null){
+                array_push($queue,$rightNode);
+                array_push($result,$rightNode->val);
+            }
+            else array_push($result,null);
+        }
+        return $result;
+    }
     function invertTree($root) {
         if($root==null){
             return null;
@@ -24,5 +87,11 @@ class Solution {
         return $root;
     }
 }
-
+$solution=new Solution();
+$tree=$solution->buildTree([1,3,2,5,3]);
+$unbuildedTree=$solution->unbuildTree($tree);
+echo json_encode($unbuildedTree)."\n";
+$invtree=$solution->invertTree($tree);
+$unbuildedinvTree=$solution->unbuildTree($invtree);
+echo json_encode($unbuildedinvTree)."\n";
 ?>
